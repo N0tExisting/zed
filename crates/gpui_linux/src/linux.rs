abbf3792d59ce7a81d94ebf3b51c1b5e7c7697df
N0tExisting <61290107+N0tExisting@users.noreply.github.com>
2026-05-31 15:32:49 +0200

    fixup!: improve the documentation

--- crates/gpui_linux/src/linux.rs
+++ crates/gpui_linux/src/linux.rs
@@ -33,9 +33,9 @@ fn new_headless() -> Rc<dyn Platform> {
     })
 }
 
-/// This can only be reached when the feature `x11`/`wayland` is enabled in gpui but not here
+/// This can only be reached when the feature `x11`/`wayland` is enabled in gpui but not here.
 /// (It shoudn't happen, but it did happen to me, so ¯\_(ツ)_/¯)
-/// panics from ´unreachable!´
+/// Panics from a ´unreachable!´.
 fn disabled_compositor(compositor: Compositor) -> Rc<dyn Platform> {
     unreachable!(
         "Got compositor `{}`, but gpui_linux was compiled without that compositor feature",
